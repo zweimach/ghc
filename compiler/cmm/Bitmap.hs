34d5ce8f08de0a1908dd8ab97bf0fadbf5a3a613
Ben Gamari <ben@smart-cactus.org>
2015-07-06 17:47:29 +0200

    Add Note describing needed strictness

--- compiler/cmm/Bitmap.hs
+++ compiler/cmm/Bitmap.hs
@@ -55,14 +55,16 @@ intsToBitmap dflags size = go 0
     oneAt :: Int -> StgWord
     oneAt i = toStgWord dflags 1 `shiftL` i
 
+    -- It is important that we maintain strictness here.
+    -- See Note [Strictness when building Bitmaps].
     go :: Int -> [Int] -> Bitmap
-    go !pos slots'
+    go !pos slots
       | size <= pos = []
       | otherwise =
         (foldr (.|.) (toStgWord dflags 0) (map (\i->oneAt (i - pos)) these)) :
           go (pos + word_sz) rest
       where
-        (these,rest) = span (< (pos + word_sz)) slots'
+        (these,rest) = span (< (pos + word_sz)) slots
 
 -- | Make a bitmap where the slots specified are the /zeros/ in the bitmap.
 -- eg. @[0,1,3], size 4 ==> 0x4@  (we leave any bits outside the size as zero,
@@ -79,18 +81,39 @@ intsToReverseBitmap dflags size = go 0
     oneAt :: Int -> StgWord
     oneAt i = toStgWord dflags 1 `shiftL` i
 
+    -- It is important that we maintain strictness here.
+    -- See Note [Strictness when building Bitmaps].
     go :: Int -> [Int] -> Bitmap
-    go !pos slots'
+    go !pos slots
       | size <= pos = []
       | otherwise =
         (foldr xor (toStgWord dflags init) (map (\i->oneAt (i - pos)) these)) :
           go (pos + word_sz) rest
       where
-        (these,rest) = span (< (pos + word_sz)) slots'
+        (these,rest) = span (< (pos + word_sz)) slots
         remain = size - pos
         init
           | remain >= word_sz = -1
           | otherwise         = (1 `shiftL` remain) - 1
+
+{-
+
+Note [Strictness when building Bitmaps]
+========================================
+
+One of the places where @Bitmap@ is used is in in building Static Reference
+Tables (SRTs) (in @CmmBuildInfoTables.procpointSRT@). In #7450 it was noticed
+that some test cases (particularly those whose C-- have large numbers of CAFs)
+produced large quantities of allocations from this function.
+
+The source traced back to 'intsToBitmap', which was lazily subtracting the word
+size from the elements of the tail of the @slots@ list and recursively invoking
+itself with the result. This resulted in large numbers of subtraction thunks
+being built up. Here we take care to avoid passing new thunks to the recursive
+call. Instead we pass the unmodified tail along with an explicit position
+accumulator, which get subtracted in the fold when we compute the Word.
+
+-}
 
 {- |
 Magic number, must agree with @BITMAP_BITS_SHIFT@ in InfoTables.h.
