5502579e61fbddfb8cfa133e64405c4c01975c37
Ben Gamari <ben@smart-cactus.org>
2015-08-19 19:59:01 +0200

    Add libunwind

---
+++ rts/Libunwind.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+struct BacktraceFrame_ {
+    const char *filename;
+    StgWord lineno;
+    uintptr_t pc;
+    const char *function;
+};
+
+typedef struct BacktraceFrame_ BacktraceFrame;
+
+struct BacktraceChunk_ {
+    StgWord n_frames;             // number of frames in this chunk
+    struct BacktraceChunk_ *next; // the chunk following this one
+    BacktraceFrame frames[];      // the actual stack frames
+};
+
+typedef struct BacktraceChunk_ BacktraceChunk;
+
+struct Backtrace_ {
+    StgWord n_frames;             // Total number of frames in the backtrace
+    BacktraceChunk frames;        // The first chunk of frames
+};
+
+typedef struct Backtrace_ Backtrace;
+
+void free_backtrace(Backtrace *bt);
+
+void print_backtrace(FILE *file, Backtrace *bt);
+
+struct LibunwindSession_;
+typedef struct LibunwindSession_ LibunwindSession;
+
+LibunwindSession *libunwind_init();
+
+Backtrace *libunwind_get_backtrace(LibunwindSession *session);
