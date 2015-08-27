c8ea2b60a52a666c94794cae8b93d9f8dd86d142
Ben Gamari <ben@smart-cactus.org>
2015-08-27 08:20:47 +0200

    Lazily lookup locations

--- rts/Libdw.c
+++ rts/Libdw.c
@@ -278,31 +278,25 @@ Backtrace *get_stg_backtrace(StgTSO *tso) {
     return bt;
 }
 
-static LocationBacktrace *libdw_lookup_backtrace(LibDwSession *session, Backtrace *bt) {
-    int len = sizeof(StgWord) + bt->n_frames * sizeof(Location);
-    LocationBacktrace *lbt = stgMallocBytes(len, "libdw_lookup_backtrace");
-    lbt->n_frames = bt->n_frames;
-    int i = 0;
-    StgPtr pc;
-    FOREACH_FRAME(pc, bt) {
-        libdw_lookup_location(session, &lbt->frames[i], pc);
-        i++;
-    }
-    return lbt;
-}
-
-LocationBacktrace *libdw_my_cap_get_backtrace(void) {
+int libdw_my_cap_lookup_location(StgPtr pc, Location *loc) {
     Capability *cap = myTask()->cap;
     if (!cap->libdw)
         cap->libdw = libdw_init();
     if (cap->libdw) {
-         Backtrace *bt = libdw_get_backtrace(cap->libdw);
-         LocationBacktrace *lbt = libdw_lookup_backtrace(cap->libdw, bt);
-         backtrace_free(bt);
-         return lbt;
+        return libdw_lookup_location(cap->libdw, loc, pc);
     } else {
-        return NULL;
+        return 1;
     }
+}
+
+Backtrace *libdw_my_cap_get_backtrace(void) {
+    Capability *cap = myTask()->cap;
+    if (!cap->libdw)
+        cap->libdw = libdw_init();
+    if (!cap->libdw)
+        return NULL;
+
+    return libdw_get_backtrace(cap->libdw);
 }
 
 void libdw_my_cap_free(void) {
