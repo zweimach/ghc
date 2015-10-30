2681a9ada6be89f0b209b7bff99db5ff598b72d3
Richard Eisenberg <eir@cis.upenn.edu>
2015-10-30 17:33:16 -0400

    more instances are undecidable now

--- libraries/base/Data/Data.hs
+++ libraries/base/Data/Data.hs
@@ -3,6 +3,7 @@
              TypeOperators, GADTs, FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE UndecidableInstances #-}   -- for instance Data (Alt f a)
 
 -----------------------------------------------------------------------------
 -- |
