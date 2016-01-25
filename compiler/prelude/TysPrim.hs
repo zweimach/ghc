f462190b52b22f1d3a01f16a78c9456fd2c30b4a
Ben Gamari <ben@smart-cactus.org>
2016-01-25 12:38:55 +0100

    Make kind of (->) polymorphic

--- compiler/prelude/TysPrim.hs
+++ compiler/prelude/TysPrim.hs
@@ -262,31 +262,17 @@ funTyConName = mkPrimTyConName (fsLit "(->)") funTyConKey funTyCon
 funTyCon :: TyCon
 funTyCon = mkFunTyCon funTyConName kind tc_rep_nm
   where
-    kind = mkFunTys [liftedTypeKind, liftedTypeKind] liftedTypeKind
-        -- You might think that (->) should have type (?? -> ? -> *), and you'd be right
-        -- But if we do that we get kind errors when saying
-        --      instance Control.Arrow (->)
-        -- because the expected kind is (*->*->*).  The trouble is that the
-        -- expected/actual stuff in the unifier does not go contra-variant, whereas
-        -- the kind sub-typing does.  Sigh.  It really only matters if you use (->) in
-        -- a prefix way, thus:  (->) Int# Int#.  And this is unusual.
-        -- because they are never in scope in the source
+    -- forall (l1 :: Levity) (l2 :: Levity).
+    -- TYPE l1 -> TYPE l2 -> *
+    kind = ForAllTy lev1
+         $ ForAllTy lev2
+         $ mkFunTys [tYPE levity1Ty, tYPE levity2Ty] liftedTypeKind
+
+    lev1, lev2 :: TyBinder
+    [lev1, lev2] = map (\var -> Named var Invisible)
+                       [levity1TyVar, levity2TyVar]
 
     tc_rep_nm = mkPrelTyConRepName funTyConName
-
--- One step to remove subkinding.
--- (->) :: * -> * -> *
--- but we should have (and want) the following typing rule for fully applied arrows
---      Gamma |- tau   :: k1    k1 in {*, #}
---      Gamma |- sigma :: k2    k2 in {*, #, (#)}
---      -----------------------------------------
---      Gamma |- tau -> sigma :: *
--- Currently we have the following rule which achieves more or less the same effect
---      Gamma |- tau   :: ??
---      Gamma |- sigma :: ?
---      --------------------------
---      Gamma |- tau -> sigma :: *
--- In the end we don't want subkinding at all.
 
 {-
 ************************************************************************
