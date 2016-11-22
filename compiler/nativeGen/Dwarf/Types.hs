ce92e53f25953ff864d208388b6a66c3a12f2a36
Ben Gamari <ben@smart-cactus.org>
2016-11-22 02:15:06 -0500

    Dwarf.Types: Use DW_CFA_same_value encoding when possible
    This is a bit smaller than the alternative, DW_CFA_val_expression. Also a bit of
    refactoring.

--- compiler/nativeGen/Dwarf/Types.hs
+++ compiler/nativeGen/Dwarf/Types.hs
@@ -430,8 +430,7 @@ pprSetUnwind :: Platform
                 -- ^ the old and new values of the register
              -> SDoc
 pprSetUnwind plat g  (_, Nothing)
-  = pprByte dW_CFA_undefined $$
-    pprLEBWord (fromIntegral $ dwarfGlobalRegNo plat g)
+  = pprUndefUnwind plat g
 pprSetUnwind _    Sp (Just (UwReg s _), Just (UwReg s' o')) | s == s'
   = if o' >= 0
     then pprByte dW_CFA_def_cfa_offset $$ pprLEBWord (fromIntegral o')
@@ -439,10 +438,10 @@ pprSetUnwind _    Sp (Just (UwReg s _), Just (UwReg s' o')) | s == s'
 pprSetUnwind plat Sp (_, Just (UwReg s' o'))
   = if o' >= 0
     then pprByte dW_CFA_def_cfa $$
-         pprLEBWord (fromIntegral $ dwarfGlobalRegNo plat s') $$
+         pprLEBRegNo plat s' $$
          pprLEBWord (fromIntegral o')
     else pprByte dW_CFA_def_cfa_sf $$
-         pprLEBWord (fromIntegral $ dwarfGlobalRegNo plat s') $$
+         pprLEBRegNo plat s' $$
          pprLEBInt o'
 pprSetUnwind _    Sp (_, Just uw)
   = pprByte dW_CFA_def_cfa_expression $$ pprUnwindExpr False uw
@@ -452,16 +451,25 @@ pprSetUnwind plat g  (_, Just (UwDeref (UwReg Sp o)))
     pprLEBWord (fromIntegral ((-o) `div` platformWordSize plat))
   | otherwise
   = pprByte dW_CFA_offset_extended_sf $$
-    pprLEBWord (fromIntegral (dwarfGlobalRegNo plat g)) $$
+    pprLEBRegNo plat g $$
     pprLEBInt o
 pprSetUnwind plat g  (_, Just (UwDeref uw))
   = pprByte dW_CFA_expression $$
-    pprLEBWord (fromIntegral (dwarfGlobalRegNo plat g)) $$
+    pprLEBRegNo plat g $$
     pprUnwindExpr True uw
+pprSetUnwind plat g  (_, Just (UwReg g' 0))
+  | g == g'
+  = pprByte dW_CFA_same_value $$
+    pprLEBRegNo plat g
 pprSetUnwind plat g  (_, Just uw)
   = pprByte dW_CFA_val_expression $$
-    pprLEBWord (fromIntegral (dwarfGlobalRegNo plat g)) $$
+    pprLEBRegNo plat g $$
     pprUnwindExpr True uw
+
+-- | Print the register number of the given 'GlobalReg' as an unsigned LEB128
+-- encoded number.
+pprLEBRegNo :: Platform -> GlobalReg -> SDoc
+pprLEBRegNo plat = pprLEBWord . fromIntegral . dwarfGlobalRegNo plat
 
 -- | Generates a DWARF expression for the given unwind expression. If
 -- @spIsCFA@ is true, we see @Sp@ as the frame base CFA where it gets
@@ -492,7 +500,7 @@ pprUnwindExpr spIsCFA expr
 pprUndefUnwind :: Platform -> GlobalReg -> SDoc
 pprUndefUnwind _    Sp = panic "pprUndefUnwind Sp" -- should never happen
 pprUndefUnwind plat g  = pprByte dW_CFA_undefined $$
-                         pprLEBWord (fromIntegral $ dwarfGlobalRegNo plat g)
+                         pprLEBRegNo plat g
 
 
 -- | Align assembly at (machine) word boundary
