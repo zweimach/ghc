403fab434a554d9af6324126abc20f6348192c1a
Ben Gamari <ben@smart-cactus.org>
2017-03-27 15:48:58 -0400

    Try again?

--- compiler/types/Unify.hs
+++ compiler/types/Unify.hs
@@ -1211,7 +1211,7 @@ ty_co_match :: MatchEnv   -- ^ ambient helpful info
             -> Coercion   -- ^ :: kind of R type of substed ty ~N R kind of co
             -> Maybe LiftCoEnv
 ty_co_match menv subst ty co lkco rkco
-  | Just ty' <- coreView ty = ty_co_match menv subst ty' co lkco rkco
+  | Just ty' <- tcView ty = ty_co_match menv subst ty' co lkco rkco
 
   -- handle Refl case:
   | tyCoVarsOfType ty `isNotInDomainOf` subst
