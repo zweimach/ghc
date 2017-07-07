d6bb161b53fd3a23d8da7313af52ec71945e36ec
Ben Gamari <ben@smart-cactus.org>
2017-07-07 19:48:22 -0400

    Fix test

--- testsuite/tests/dynlibs/T13702a.hs
+++ testsuite/tests/dynlibs/T13702a.hs
@@ -5,7 +5,7 @@ import System.Posix.DynamicLinker
 
 main :: IO ()
 main = do
-    dl <- dlopen "T13702" [RTLD_NOW]
+    dl <- dlopen "./T13702" [RTLD_NOW]
     funptr <- dlsym dl "hello" :: IO (FunPtr (IO ()))
     mkAction funptr
 
