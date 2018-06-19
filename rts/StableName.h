a3dd69b619d21f21a63f4012ff27c1022773ee7b
Ömer Sinan Ağacan <omeragacan@gmail.com>
2018-06-19 09:14:44 +0300

    Implement stable name table sweeping
    Fixes #48
    
    - Export some stuff from Stable.c to be reused in NonMovingSweep.c
    - We now sweep the stable name table after mark phase, similar to large
      object lists and mut_list.
    
    The code is inefficient (calls multiple variants of isAlive) and won't
    work in the concurrent implementation.

--- rts/StableName.h
+++ rts/StableName.h
@@ -23,6 +23,30 @@ void    updateStableNameTable ( bool full );
 void    stableNameLock            ( void );
 void    stableNameUnlock          ( void );
 
+extern snEntry *stable_name_table;
+extern unsigned int SNT_size;
+
+#define FOR_EACH_STABLE_NAME(p, CODE)                                   \
+    do {                                                                \
+        snEntry *p;                                                     \
+        snEntry *__end_ptr = &stable_name_table[SNT_size];              \
+        for (p = stable_name_table + 1; p < __end_ptr; p++) {           \
+            /* Internal pointers are free slots.  */                    \
+            /* If p->addr == NULL, it's a */                            \
+            /* stable name where the object has been GC'd, but the */   \
+            /* StableName object (sn_obj) is still alive. */            \
+            if ((p->addr < (P_)stable_name_table ||                     \
+                 p->addr >= (P_)__end_ptr))                             \
+            {                                                           \
+                /* NOTE: There is an ambiguity here if p->addr == NULL */ \
+                /* it is either the last item in the free list or it */ \
+                /* is a stable name whose pointee died. sn_obj == NULL */ \
+                /* disambiguates as last free list item. */             \
+                do { CODE } while(0);                                   \
+            }                                                           \
+        }                                                               \
+    } while(0)
+
 #if defined(THREADED_RTS)
 // needed by Schedule.c:forkProcess()
 extern Mutex stable_name_mutex;
