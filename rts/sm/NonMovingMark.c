6e05b49a52e59ca56a4c74cfc6c00c18d1641f51
Ben Gamari <ben@smart-cactus.org>
2018-07-07 21:55:49 -0400

    Untag pointers in mark queue

--- rts/sm/NonMovingMark.c
+++ rts/sm/NonMovingMark.c
@@ -91,10 +91,10 @@ void mark_queue_push_closure (MarkQueue *q,
     MarkQueueEnt ent = {
         .type = MARK_CLOSURE,
         .mark_closure = {
-            .p = p,
-            .origin = origin_closure,
+            .p = UNTAG_CLOSURE(p),
+            .origin = UNTAG_CLOSURE(origin_closure),
             .origin_field = origin_field,
-            .origin_value = p
+            .origin_value = UNTAG_CLOSURE(p)
         }
     };
     mark_queue_push(q, &ent);
