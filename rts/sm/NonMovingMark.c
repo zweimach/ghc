a896a471cef721ac041c11a12b4b23e76fc1535d
Ben Gamari <ben@smart-cactus.org>
2018-07-09 17:29:46 -0400

    Fix warning

--- rts/sm/NonMovingMark.c
+++ rts/sm/NonMovingMark.c
@@ -765,7 +765,7 @@ mark_closure (MarkQueue *queue, StgClosure *p)
         } else {
             struct nonmoving_segment *seg = nonmoving_get_segment((StgPtr) p);
             nonmoving_block_idx block_idx = nonmoving_get_block_idx((StgPtr) p);
-            if (p >= nonmoving_segment_get_block(seg, seg->next_free_snap)
+            if (p >= (StgClosure *) nonmoving_segment_get_block(seg, seg->next_free_snap)
                 || nonmoving_get_mark_bit(seg, block_idx)) {
                 return;
             }
