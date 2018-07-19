af7e860f618f6d8e41cf1a2e07b7ac4d22c55cb7
Ömer Sinan Ağacan <omeragacan@gmail.com>
2018-07-19 12:43:35 +0300

    Simplify large object marking
    Previously we used a hash table to keep marked large objects. We now use
    the bdescr flag BF_MARKED to mark large objects. This removes hash
    tables in a bunch of places.
    
    Hash tables are still used for marking static objects.

--- rts/sm/NonMovingSweep.h
+++ rts/sm/NonMovingSweep.h
@@ -14,10 +14,10 @@
 GNUC_ATTR_HOT void nonmoving_sweep(void);
 
 // Remove unmarked entries in oldest generation mut_lists
-void nonmoving_sweep_mut_lists(HashTable *marked_objects);
+void nonmoving_sweep_mut_lists(void);
 
 // Remove unmarked entries in oldest generation scavenged_large_objects list
-void nonmoving_sweep_large_objects(HashTable *marked_objects);
+void nonmoving_sweep_large_objects(void);
 
 // Remove dead entries in the stable name table
-void nonmoving_sweep_stable_name_table(HashTable *marked_objects);
+void nonmoving_sweep_stable_name_table(void);
