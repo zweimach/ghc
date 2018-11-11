fe36b239564b9f397954b9ea62a76011f7e017ef
Ben Gamari <ben@smart-cactus.org>
2018-11-10 19:15:44 -0500

    Add some simple diagnostics for characterising fragmentation

---
+++ rts/sm/NonMovingCensus.h
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The GHC Team, 1998-2018
+ *
+ * Non-moving garbage collector and allocator: Accounting census
+ *
+ * ---------------------------------------------------------------------------*/
+
+#pragma once
+
+void nonmoving_print_allocator_census(void);
