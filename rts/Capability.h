b2913f5510c524d6b15b66b2f783ad3ae0092384
Ben Gamari <ben@smart-cactus.org>
2019-10-20 00:56:38 -0400

    Fixes

--- rts/Capability.h
+++ rts/Capability.h
@@ -406,6 +406,8 @@ INLINE_HEADER bool emptyInbox(Capability *cap);
  * INLINE functions... private below here
  * -------------------------------------------------------------------------- */
 
+WARD_NEED(may_take_sm_lock)
+WARD_NEED(may_call_sm)
 EXTERN_INLINE void
 recordMutableCap (const StgClosure *p, Capability *cap, uint32_t gen)
 {
@@ -425,6 +427,8 @@ recordMutableCap (const StgClosure *p, Capability *cap, uint32_t gen)
     *bd->free++ = (StgWord)p;
 }
 
+WARD_NEED(may_take_sm_lock)
+WARD_NEED(may_call_sm)
 EXTERN_INLINE void
 recordClosureMutated (Capability *cap, StgClosure *p)
 {
